5df27778f93e1d89a1ed1d478d8d4cd809b40a9f
071yoon <yeonggilol@gmail.com>
2021-12-05 20:33:08 +0900

    2021_12_05_2033

---
+++ C++/14. backtracking/15651/15651.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int n,m,tmp;
+int arr[8] = {0};
+bool visited[9] = {0};
+
+void dfs(int cnt){
+    if(cnt == m){//when arrival
+        for(int i = 0; i < m; i++){
+            printf("%d ", arr[i]);//print saved value
+        }
+        printf("\n");
+        return;
+    }
+    for(int i = 1; i <= n; i++){//if not M
+        if(!visited[i]){//if not visited
+            if (i == n && cnt == m)
+                visited[i] = true;//visited
+            arr[cnt] = i;//count = visit
+            dfs(cnt + 1);//next value
+            visited[i] = false;//backtracking
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+    dfs(0);
+}
