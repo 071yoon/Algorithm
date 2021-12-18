2dfe01c87944aff0505a2cb36186910492abb846
071yoon <yeonggilol@gmail.com>
2021-12-19 00:30:25 +0900

    2021_12_19_0030

--- C++/16. greedy/1931/1931.cpp
+++ C++/16. greedy/1931/1931.cpp
@@ -1,30 +1,39 @@
 #include <stdio.h>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-bool compare(pair<int, int> a, pair<int, int> b){
-	return a.second < b.second;
+bool cmp(pair<int, int> a, pair<int, int> b){
+	if (a.first == b.first)
+		return a.second < b.second;
+	else
+		return a.first < b.first;
 }
 
 int main(){
-	int cnt, start, end;
+	int cnt, num1, num2;
 
 	vector <pair<int, int> > v;
+	
 	scanf("%d", &cnt);
 	for(int i = 0; i < cnt; i++){
-		scanf("%d %d", &start, &end);
-		v.push_back(make_pair(end, start));
+		scanf("%d %d", &num1, &num2);
+		v.push_back(make_pair(num2, num1));
 	}
 
-	sort(v.begin(), v.end(), compare);
-	int strt = v[0].first;
-	int count = 1;
+	sort(v.begin(), v.end());
+	int starter = v[0].first;
+	int counter = 1;
+
 	for (int i = 1; i < cnt; i++){
-		if (strt <= v[i].second){
-			count++;
-			strt = v[i].first;
+		if (starter <= v[i].second){
+			counter++;
+			starter = v[i].first;
 		}
 	}
-	printf("%d", count);
+
+	printf("%d", counter);
+	return (0);
 }
