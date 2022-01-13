a95758498362adf1db90a73387d0ea6fdc9d3816
071yoon <yeonggilol@gmail.com>
2022-01-13 17:15:52 +0900

    2022_01_13_1715

--- C++/Kakao/2021/level1/new_id.cpp
+++ C++/Kakao/2021/level1/new_id.cpp
@@ -4,24 +4,49 @@
 using namespace std;
 
 string solution(string new_id){
-	string answer;
 
-	for(int i = 0; i < new_id.size(); i++){
+	for(int i = 0; i < new_id.size(); i++){//1
 		if (new_id[i] >= 'A' && new_id[i] <= 'Z')
 			new_id[i] = new_id[i] - 'A' + 'a';
 	}
-	for(int i = 0; i < new_id.size(); i++){
+	for(int i = 0; i < new_id.length(); i++){//2
 		if (!(new_id[i] == '.' || new_id[i] == '-' || new_id[i] == '_' ||
 			(new_id[i] >= 'a' && new_id[i] <= 'z') || 
 			(new_id[i] >= '0' && new_id[i] <= '9'))){
-				int j;
-				for(j = i; j < new_id.size() - 1; j++){
-					new_id[j] = new_id[j + 1];
-				}
-				new_id[j] = '\0';
-				i--;
+			new_id.erase(new_id.begin() + i);
+			i--;
 		}
 	}
+	for(int i = 0; i < new_id.length(); i++){//3
+		if (new_id[i] == '.'){
+			while (new_id[i + 1] == '.'){
+				new_id.erase(new_id.begin() + i + 1);
+			}
+		}
+	}
+	if (new_id.front() == '.')
+		new_id.erase(new_id.begin());
+	
+	if (new_id.back() == '.')
+		new_id.erase(new_id.end() - 1);
+	
+	if (new_id.length() == 0){//5
+		new_id = 'a';
+	}
+	if (new_id.length() >= 16){//6
+		while(new_id.length() != 15){
+            new_id.erase(new_id.begin() + 15);
+        }
+	}
+	if (new_id.back() == '.')
+		new_id.erase(new_id.end() - 1);
+	
+    if (new_id.length() <= 2){
+        while(new_id.length() != 3){
+            new_id += new_id.back();
+        }
+    }
+
 	return new_id;
 }
 int main(){
